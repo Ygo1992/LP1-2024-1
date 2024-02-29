2b51645f5cd73bf6359f46ddd17766f29e0b1d70
Ygo Brito <ygo1992@gmail.com>
2024-02-28 20:22:01 -0400

    Primeiro contato com callback e promises

---
+++ Callback e Promises/matematica.js
@@ -0,0 +1,12 @@
+//funcao que verifica se o numero é par
+export const ehPar = (numero) => {
+/*    if(numero % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+*/
+    return numero % 2 === 0? true : false;
+} 
+
+//export { ehPar }
