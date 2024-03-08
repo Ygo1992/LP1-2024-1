585bcf71806e0e7465f584cc660271c7c4fab89b
Ygo Brito <ygo1992@gmail.com>
2024-03-07 22:27:26 -0400

    Trabalhando com async/await

--- Callback e Promises/index.js
+++ Callback e Promises/index.js
@@ -1,13 +1,35 @@
-import { ehPar, ehPar2 } from "./matematica.js";
+import { ehPar4 } from "./matematica.js";
 
 //v1
-console.log(ehPar('asdasd'));
+//console.log(ehPar('asdasd'));
 
-//v2
-ehPar2(1)
-.then((resultado) => {
+//v3 - Promises
+/*ehPar3(10)
+.then((resultado) => console.log("Promessa Resolvida: ", resultado))
+.catch((erro) => { console.log(erro); })
+
+ehPar3('blabla')
+.then((resultado) => console.log("Promessa Resolvida!"))
+.catch((erro) => { console.log(erro); })
+*/
+
+//v4, usando async/await
+/*
+const run = async () => {
+    var resultado = await ehPar4(10);
     console.log(resultado);
-})
-.catch((erro) => {
-    console.log(erro);
-})
+
+    resultado = await ehPar4('blabla');
+    console.log(resultado);
+}
+run();
+*/
+
+//v4, usando async/await - IIFE (Immediately Invoked Function Expression)
+;(async () => {
+    var resultado = await ehPar4(10);
+    console.log(resultado);
+
+    resultado = await ehPar4('blabla');
+    console.log(resultado);
+})()
